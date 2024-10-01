dfd78844e58a66e6f53a60f5e3209a079f8e7026
Susana Duque <su.duquem@gmail.com>
2024-09-30 22:25:32 -0500

    Use the information from the usePokemonDetails hook in the Card component

--- src/Components/PokemonCard.tsx
+++ src/Components/PokemonCard.tsx
@@ -1,52 +1,131 @@
-import { useEffect, useState } from 'react';
-import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Typography,
+} from '@mui/material';
 import { PokemonWithImage } from '../interfaces/pokemon.interfaces';
-import { getColorFromUrl } from '../utils/color';
+import usePokemonDetails from '../hooks/usePokemonDetails';
 
 interface PokemonCardProps {
   pokemon: PokemonWithImage; // It only receives 1 object
 }
+
 const PokemonCard = ({ pokemon }: PokemonCardProps) => {
-  const { name, imageUrl, pokemonIndex } = pokemon;
-  const [pokemonColor, setPokemonColor] = useState<string | null>(null);
+  const { name, pokemonIndex } = pokemon;
+  const { pokemonDetails, isLoading } = usePokemonDetails({
+    pokemonName: name,
+  });
 
-  const getPokemonColor = async () => {
-    const color = await getColorFromUrl(imageUrl);
-    if (color) setPokemonColor(color);
-  };
-
-  useEffect(() => {
-    getPokemonColor();
-  }, []);
+  const { height, weight, types, image, color } = pokemonDetails ?? {};
 
   return (
-    <Card sx={{ backgroundColor: pokemonColor }}>
-      {/* To add a hover effect to the card: <CardActionArea></CardActionArea> */}
-      <CardMedia
-        component='img'
-        image={imageUrl}
-        title={name}
-        sx={{ height: 100, objectFit: 'contain' }}
-      />
-      <CardContent>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            flexDirection: 'column',
-            alignItems: 'center',
-            color: 'white',
-          }}
-        >
-          <Typography sx={{ textTransform: 'capitalize' }}>
-            #{pokemonIndex}
-          </Typography>
-          <Typography sx={{ textTransform: 'uppercase', fontWeight: 'bold' }}>
-            {name}
-          </Typography>
-        </Box>
-      </CardContent>
-    </Card>
+    <>
+      {isLoading || !pokemonDetails ? (
+        <CircularProgress color='warning' />
+      ) : (
+        <Card sx={{ backgroundColor: color }}>
+          {/* To add a hover effect to the card: <CardActionArea></CardActionArea> */}
+          <CardMedia
+            component='img'
+            image={image}
+            title={name}
+            sx={{ height: 100, objectFit: 'contain', marginTop: 2 }}
+          />
+          <CardContent
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1,
+            }}
+          >
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: 'white',
+                gap: 2,
+              }}
+            >
+              <Typography
+                sx={{
+                  display: 'flex',
+                  textTransform: 'capitalize',
+                  flex: 1,
+                  justifyContent: 'center',
+                  backgroundColor: 'PaleVioletRed',
+                }}
+              >
+                #{pokemonIndex}
+              </Typography>
+              <Typography
+                sx={{
+                  display: 'flex',
+                  textTransform: 'uppercase',
+                  fontWeight: 'bold',
+                  justifyContent: 'center',
+                  flex: 2,
+                }}
+              >
+                {name}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                alignItems: 'center',
+                color: 'white',
+              }}
+            >
+              {types?.map((item) => (
+                <Typography
+                  key={item.type.name}
+                  sx={{
+                    textTransform: 'capitalize',
+                    backgroundColor: 'Plum',
+                  }}
+                >
+                  {item.type.name}
+                </Typography>
+              ))}
+            </Box>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
+                alignItems: 'center',
+                color: 'white',
+              }}
+            >
+              {weight && (
+                <Typography
+                  sx={{
+                    textTransform: 'capitalize',
+                    backgroundColor: 'LightCoral',
+                  }}
+                >
+                  {weight}kg
+                </Typography>
+              )}
+              {height && (
+                <Typography
+                  sx={{
+                    textTransform: 'capitalize',
+                    backgroundColor: 'LightCoral',
+                  }}
+                >
+                  {height}m
+                </Typography>
+              )}
+            </Box>
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 };
 
